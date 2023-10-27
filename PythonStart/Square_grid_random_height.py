814dc711396ba89984f303bc18fa5171ce334afa
Seldon552 <m.bouquot@rubika-edu.ca>
2023-10-27 18:20:53 -0400

    square_grid_random_height

---
+++ PythonStart/Square_grid_random_height.py
@@ -0,0 +1,16 @@
+import pymel.core as pm
+from random import random
+
+def create_grid(cubes_per_side, distanceX=3,distanceZ=3):
+    index_name=0
+    for each_x in range(cubes_per_side):
+        index_name =index_name+1
+        for each_z in range(cubes_per_side):
+            random_height = int(random()*10)
+            cube_transform, cube_creation = pm.polyCube(name=f'Cube{index_name}')
+            pm.setAttr(f'{cube_transform}.translateX', distanceX *each_x)
+            pm.setAttr(f'{cube_transform}.translateZ', distanceZ * each_z)
+            pm.setAttr(f'{cube_transform}.scaleY', random_height)
+
+
+create_grid(6)

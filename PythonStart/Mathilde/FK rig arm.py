3a44b50173c8f29885af81f7011beeac305a4c00
Seldon552 <m.bouquot@rubika-edu.ca>
2023-11-03 19:33:42 -0400

    fine
    ok

---
+++ PythonStart/Mathilde/FK rig arm.py
@@ -0,0 +1,7 @@
+import RMPY.rig.rigBase
+from RMPY.rig import rigIKFK
+import RMPY.rig.rigFK
+import pymel.core as pm
+from RMPY.rig.biped.rig import rigRibonTwistJoint
+ArmModel = ArmModel()
+Arm= Arm()
